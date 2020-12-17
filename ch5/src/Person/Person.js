b44cd2a98b8a9aa814a793eae16497e600097cf6
Sunny Lin <sunnylin9999@gmail.com>
2020-12-17 18:49:22 +0800

    Update Person.js, add CSS modules

--- ch5/src/Person/Person.js
+++ ch5/src/Person/Person.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import './Person.css';
+import cssClasses from './Person.css';
 
 const person = (props) => {
     return (
-        <div className="Person">
+        <div className={cssClasses.Person}>
             <p onClick={props.clicked}>I'm {props.name}, {props.age} years old!</p>
             <p>{props.children}</p>
 
