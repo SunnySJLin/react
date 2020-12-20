5876610da34f01083faddab57cea6e842e1b660e
Sunny Lin <sunnylin9999@gmail.com>
2020-12-21 02:40:58 +0800

    Add Update Lifecycle Hooks for state

--- ch7/src/containers/App.js
+++ ch7/src/containers/App.js
@@ -37,6 +37,15 @@ class App extends Component {
     console.log('[App.js] componentDidMount');
   }
 
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[App.js] shouldComponentUpdate');
+    return true;
+  }
+
+  componentDidUpdate() {
+    console.log('[App.js] componentDidUpdate');
+  }
+
   changeNameHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex(p => {
       return p.id === id;
