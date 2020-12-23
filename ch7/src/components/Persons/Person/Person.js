a8767058500b91eddd86c2fdecc13eef9ea3f7e8
Sunny Lin <sunnylin9999@gmail.com>
2020-12-23 17:03:58 +0800

    Using Refs
    ~16.3: pass a function
    16.3~: using React object

--- ch7/src/components/Persons/Person/Person.js
+++ ch7/src/components/Persons/Person/Person.js
@@ -5,6 +5,17 @@ import withClass from '../../../hoc/withClass';
 import PropTypes from 'prop-types';
 
 class Person extends Component {
+  constructor(props) {
+    super(props);
+    this.inputElementRef = React.createRef(); // React object
+  }
+
+  componentDidMount() {
+    // document.querySelector('input').focus(); // select the first element only
+    // this.inputElement.focus(); // pass a function
+    this.inputElementRef.current.focus();
+  }
+
   render() {
     console.log('[Perosn.js] rendering...');
     
@@ -13,7 +24,9 @@ class Person extends Component {
         <p onClick={this.props.clicked}>I'm {this.props.name}, {this.props.age} years old!</p>
         <p>{this.props.children}</p>
         <input 
-          type="text" 
+          type="text"
+          // ref={ (inputEl) => {this.inputElement = inputEl} } // pass a function
+          ref={this.inputElementRef}
           onChange={this.props.changed} 
           value={this.props.name}
           />
