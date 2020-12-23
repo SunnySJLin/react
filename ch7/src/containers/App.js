2a8e8322e5e6c1c7e4b5a67b835cbdf7f166f41b
Sunny Lin <sunnylin9999@gmail.com>
2020-12-23 11:33:17 +0800

    Change WithClass component to withClass function definition
    Return a normal function and the function return is a functional component

--- ch7/src/containers/App.js
+++ ch7/src/containers/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import cssClasses from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
-import WithClass from '../hoc/WithClass';
+import withClass from '../hoc/withClass';
+import Aux from '../hoc/Auxilliary';
 
 class App extends Component {
   constructor(props) {
@@ -96,7 +97,7 @@ class App extends Component {
     }
 
     return (
-      <WithClass cssClass={cssClasses.App}>
+      <Aux>
         <button 
           onClick={() => {
             this.setState({
@@ -116,9 +117,9 @@ class App extends Component {
           />
         ) : null}
         {persons}
-      </WithClass>
+      </Aux>
     );
   }
 }
 
-export default App;
+export default withClass(App, cssClasses.App);
--- ch7/src/hoc/WithClass.js
+++ ch7/src/hoc/WithClass.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const withClass = props => (
-  <div className={props.cssClass}>
-    {props.children}
-  </div>
-);
+const withClass = (WrappedComponent, cssClass) => {
+  return props => (   
+    <div className={cssClass}>
+      <WrappedComponent {...props}/>
+    </div>
+  );
+  //return a normal function and the function return is a functional component
+};
 
 export default withClass;
