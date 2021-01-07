26822c91931030baa7ec836544509317ac363d18
Sunny SJ Lin <sunnylin9999@gmail.com>
2021-01-07 14:49:48 +0800

    Add reusable navigation items

---
+++ ch8/burger-builder/src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import cssClasses from './NavigationItems.css';
+import NavigationItem from '../NavigationItems/NavigationItem/NavigationItem';
+
+const navigationItems = () => (
+  <ul className={cssClasses.NavigationItems}>
+    <NavigationItem link="/" active>Burger Builder</NavigationItem>
+    <NavigationItem link="/">Checkout</NavigationItem>
+  </ul>
+);
+
+export default navigationItems;
