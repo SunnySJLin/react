e06b12eab2012edf8f7ef4d38011ffeb85330c34
Sunny SJ Lin <sunnylin9999@gmail.com>
2021-01-08 14:43:01 +0800

    Reusing the Backdrop in SideDrawer
    When clicking the Backdrop, the SideDrawer will hide
    Using SideDrawer CSS to play an animation, conditionally attached open and close CSS classes

--- ch8/burger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
+++ ch8/burger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,18 +2,28 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import cssClasses from './SideDrawer.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Auxiliary from '../../../hoc/Auxiliary';
 
-const sideDrawer = () => {
+const sideDrawer = (props) => {
+  let attachedClasses = [cssClasses.SideDrawer, cssClasses.Close];
+  if(props.open) {
+    attachedClasses = [cssClasses.SideDrawer, cssClasses.Open];
+  }
+
   return(
-    <div className={cssClasses.SideDrawer}>
-      <div className={cssClasses.Logo}>
-        <Logo />
+    <Auxiliary>
+      <Backdrop show={props.open} clicked={props.closed}/>
+      <div className={attachedClasses.join(' ')}>
+        <div className={cssClasses.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
       </div>
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    </Auxiliary>
   );
-}
+};
 
 export default sideDrawer;
