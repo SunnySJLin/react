8165626248e1429e2827a535ec64d6dca2d117c6
Sunny SJ Lin <sunnylin9999@gmail.com>
2021-01-11 18:42:22 +0800

    Add a Http Request to GET data using Axios
    Creating a Http Request to GET data
    Rendering fetched data
    Transforming data

--- ch9/src/containers/Blog/Blog.js
+++ ch9/src/containers/Blog/Blog.js
@@ -4,15 +4,40 @@ import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import axios from 'axios';
 
 class Blog extends Component {
+    state = {
+        posts: []
+    }
+
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                const posts = response.data.slice(0, 4);
+                const updatedPosts = posts.map(post => {
+                   return {
+                       ...post,
+                        author: 'SunnysjLin'
+                   }
+                });
+                this.setState({posts: updatedPosts});
+                // console.log(response.data);
+            });
+    }
+
     render () {
+        const posts = this.state.posts.map(post => {
+            return <Post 
+                        key={post.id} 
+                        title={post.title}
+                        author={post.author}/>;
+        });
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
                     <FullPost />
