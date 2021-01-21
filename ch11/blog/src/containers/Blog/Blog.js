d3a8b34a3e0ba3b55b0a0faae9e4d24215f01058
Sunny SJ Lin <sunnylin9999@gmail.com>
2021-01-21 16:40:05 +0800

    Add dynamic nested routes
    Changing the Posts Link path from "/" to "/posts/"
    
    Changing the Route tag order, NewPost is upper than Posts
    
    In the Blog, we route to Posts without exact Prop, then the Posts route to the FullPost with exact
    
    Changing Route path by using Props match object's URL property and merge to id
    
    The componentDidUpdate will be called because the Props changed

--- ch11/blog/src/containers/Blog/Blog.js
+++ ch11/blog/src/containers/Blog/Blog.js
@@ -4,7 +4,6 @@ import { Route, NavLink, Switch } from 'react-router-dom';
 import './Blog.css';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
-import FullPost from './FullPost/FullPost';
 
 class Blog extends Component {
     render () {    
@@ -17,7 +16,7 @@ class Blog extends Component {
                             {/* <li><a href="/">Home</a></li>
                             <li><a href="/new-post">New Post</a></li> */}
                             <li><NavLink 
-                                to="/"
+                                to="/posts/"
                                 exact
                                 activeClassName="my-active"
                                 activeStyle={{
@@ -35,9 +34,8 @@ class Blog extends Component {
                 {/* <Route path="/" exact render={() => <h1>Home</h1>} />
                 <Route path="/" render={() => <h1>Home 2</h1>} /> */}
                 <Switch>
-                    <Route path="/" exact component={Posts} />
                     <Route path="/new-post" component={NewPost} />
-                    <Route path="/:id" component={FullPost} />
+                    <Route path="/posts" component={Posts} />
                 </Switch>
             </div>
         );
