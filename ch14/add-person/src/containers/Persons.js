5f84b0f1a1eba6cc61295d29b550dd9688dc9baa
Sunny Lin <sunnylin9999@gmail.com>
2021-02-10 17:00:35 +0800

    Combining local UI state and Redux
    Local UI state for information that really is only relevant AddPerson component.
    
    The global state managed through Redux for information which is important for entire application.

--- ch14/add-person/src/containers/Persons.js
+++ ch14/add-person/src/containers/Persons.js
@@ -6,6 +6,7 @@ import AddPerson from '../components/AddPerson/AddPerson';
 import * as actionTypes from '../store/actions';
 
 class Persons extends Component {
+    
     render () {
         return (
             <div>
@@ -30,7 +31,7 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
     return {
-        onAddedPerson: () => dispatch({type: actionTypes.ADD_PERSON}),
+        onAddedPerson: (name, age) => dispatch({type: actionTypes.ADD_PERSON, personData: {name: name, age: age}}),
         onRemovedPerson: (id) => dispatch({type: actionTypes.REMOVE_PERSON, personId: id})
     }
 };
