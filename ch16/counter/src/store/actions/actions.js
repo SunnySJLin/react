9bcf0556c5ceab413406324184111d051fc0991c
Sunny Lin <sunnylin9999@gmail.com>
2021-02-23 10:26:58 +0800

    Handling asynchronous code using Redux-Thunk

--- ch16/counter/src/store/actions/actions.js
+++ ch16/counter/src/store/actions/actions.js
@@ -31,11 +31,24 @@ export const subtract = (value) => {
   };
 };
 
-export const storeResult = (res) => {
+export const saveResult = (res) => {
+  // when using 'redux-thunk'
+  // => It's synchronous action and only synchronous actions may edit the store.
   return {
     type: STORE_RESULT,
     result: res
   };
+};
+
+export const storeResult = (res) => {
+  // When using 'redux-thunk'
+  // => Never make it to the store, only use storeResult as a utility step in-between to run asynchronous code.
+  // => Required to run on a lot of actions and dispatch the synchronous action to change stae in the store.
+  return dispatch =>  {
+    setTimeout(() => {
+      dispatch(saveResult(res));
+    }, 2000);
+  }
 };
 
 export const deleteResult = (resElId) => {
