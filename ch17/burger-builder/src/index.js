7af3638cd12501decea2f7c94a207a0c3cf426d6
Sunny Lin <sunnylin9999@gmail.com>
2021-02-24 12:03:59 +0800

    Add Redux DevTools Extension

--- ch17/burger-builder/src/index.js
+++ ch17/burger-builder/src/index.js
@@ -4,13 +4,15 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
-
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import reducer from './store/reducer';
 
-const store = createStore(reducer);
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 const app = (
   <Provider store={store}>
