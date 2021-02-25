32104573605b0cacd48aa8f7855f626e4dda8bd1
Sunny Lin <sunnylin9999@gmail.com>
2021-02-25 18:16:26 +0800

    Connecting order actions and order reducer and combine reducers
    Add the Order action of purchase success or fail or starting dispatch on Checkout page.
    
    Add asynchronous code of sending HTTP requests.
    
    Connect Contact Data container and Order action.
    
    Add Order Reducer to work on Order action.
    
    Fix refresh the Checkout page problem. If ingredients is not available, then not to redirect.
    
    Combine burger builder reducer and order reducer.
    
    Adjust the dropdown value of deliveryMethod by default to 'fastest'.

--- ch17/burger-builder/src/containers/Checkout/ContactData/ContactData.js
+++ ch17/burger-builder/src/containers/Checkout/ContactData/ContactData.js
@@ -6,6 +6,8 @@ import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import Input from '../../../components/UI/Input/Input';
 import axios from '../../../axios-orders';
+import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler'
+import * as actionCreators from '../../../store/actions/index';
 
 class ContactData extends Component {
   state = {
@@ -92,13 +94,11 @@ class ContactData extends Component {
         valid: true
       }
     },
-    formIsValid: false,
-    loading: false
+    formIsValid: false
   }
 
   orderHandler = (event) => {
     event.preventDefault(); // not to reload the page
-    this.setState({loading: true});
     const formData = {};
     for(let formElementId in this.state.orderForm) {
       formData[formElementId] = this.state.orderForm[formElementId].value;
@@ -109,16 +109,7 @@ class ContactData extends Component {
       orderData: formData
     }
 
-    axios.post('/orders.json', order)
-      .then(response => {
-        // console.log(response);
-        this.setState({loading: false});
-        this.props.history.push('/');
-      })
-      .catch(error => {
-        // console.log(error);
-        this.setState({loading: false});
-      });
+    this.props.onOrderSubmit(order);
   }
 
   checkValidity(value, rules) {
@@ -202,7 +193,7 @@ class ContactData extends Component {
           <Button btnType="Success" disabled={!this.state.formIsValid}>ORDER</Button>
         </form>
     );
-    if(this.state.loading) {
+    if(this.props.loading) {
       form = <Spinner />;
     }
 
@@ -217,9 +208,16 @@ class ContactData extends Component {
 
 const mapStateToProps = state => {
   return {
-    ings: state.ingredients,
-    price: state.totalPrice
+    ings: state.burgerBuilder.ingredients,
+    price: state.burgerBuilder.totalPrice,
+    loading: state.order.loading
   }
 };
 
-export default connect(mapStateToProps)(ContactData);
+const mapDispatchToProps = dispatch => {
+  return {
+    onOrderSubmit: (orderData) => dispatch(actionCreators.purchaseBurger(orderData))
+  }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(ContactData, axios));
