8ed77d02441b994ea68b9da0f0e58a41f5c52070
Sunny Lin <sunnylin9999@gmail.com>
2021-03-03 12:25:34 +0800

    Add getting a Token from Firebase back-end for sign up
    Get a Token from the back-end, here use Firebase Authentication.
    
    Reference: Sign up with email / password (https://firebase.google.com/docs/reference/rest/auth#section-create-email-password)

--- ch18/burger-builder/src/store/actions/auth.js
+++ ch18/burger-builder/src/store/actions/auth.js
@@ -1,4 +1,5 @@
 import * as actionTypes from './actionTypes';
+import axios from 'axios';
 
 export const authStart = () => {
   return {
@@ -24,5 +25,18 @@ export const auth = (email, password) => {
   return dispatch =>  {
     dispatch(authStart());
     console.log(email, password);
+
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken	: true
+    }
+    axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyChlkIOPmYlrL2fgSN-U_yWjwJ9x5DWb1Q', authData)
+      .then(response => {
+        dispatch(authSuccess(response.data));
+      })
+      .catch(error => {
+        dispatch(authFail(error));
+      })
   };
 };
