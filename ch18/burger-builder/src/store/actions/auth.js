d3a81d63126ec048fa2caa2c8f67b8575a617ad2
Sunny Lin <sunnylin9999@gmail.com>
2021-03-03 17:18:58 +0800

    Add Reducer for storing the Token
    Store token to be able in the future to also access resources on server which are protected.
    
    Remove API KEY to [YOUR_API_KEY].
    
    Add a Spinner when authenticating and store potential errors in Store.

--- ch18/burger-builder/src/store/actions/auth.js
+++ ch18/burger-builder/src/store/actions/auth.js
@@ -8,10 +8,11 @@ export const authStart = () => {
   };
 };
 
-export const authSuccess = (authData) => {
+export const authSuccess = (token, userId) => {
   return {
     type: actionTypes.AUTH_SUCCESS,
-    authData: authData
+    idToken: token,
+    userId: userId
   };
 };
 
@@ -32,20 +33,20 @@ export const auth = (email, password, isSignup) => {
       returnSecureToken	: true
     };
 
-    let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyChlkIOPmYlrL2fgSN-U_yWjwJ9x5DWb1Q';
+    let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[YOUR_API_KEY]';
     if(!isSignup) {
-      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyChlkIOPmYlrL2fgSN-U_yWjwJ9x5DWb1Q';
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=[YOUR_API_KEY]';
     }
     console.log(url);
 
     axios.post(url, authData)
       .then(response => {
-        console.log(response.data);
-        dispatch(authSuccess(response.data));
+        console.log(response);
+        dispatch(authSuccess(response.data.idToken, response.data.localId));
       })
       .catch(err => {
         console.log(err);
-        dispatch(authFail(err));
+        dispatch(authFail(err.response.data.error));
       });
   };
 };
