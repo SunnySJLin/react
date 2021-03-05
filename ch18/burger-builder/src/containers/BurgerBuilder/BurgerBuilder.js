252ab8ccd0586317ac41823f70f09d3f7f6bbcb1
Sunny Lin <sunnylin9999@gmail.com>
2021-03-05 10:22:11 +0800

    Forwarding unauthenticated users
    When the user did login successfully, we redirect away from the authentication form to Burger Builder page, using Redirect component.
    
    For unauthenticated users need to sign up to continue checking out the burger order.
    
    Check the user is authenticated or not before starting to purchase, otherwise redirect to authenticate page using React Router push method.

--- ch18/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ ch18/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -38,7 +38,11 @@ class BurgerBuilder extends Component {
   }
 
   purchaseHandler = () => {
-    this.setState({purchasing: true});
+    if(this.props.isAuthenticated) {
+      this.setState({purchasing: true});
+    } else {
+      this.props.history.push('/auth');
+    }
   }
 
   purchaseCancelHandler = () => {
@@ -72,7 +76,8 @@ class BurgerBuilder extends Component {
             disabledInfo={disabledInfo}
             price={this.props.price}
             purchasable={this.updatePurchaseState(this.props.ings)}
-            ordered={this.purchaseHandler} />
+            ordered={this.purchaseHandler}
+            isAuth={this.props.isAuthenticated} />
         </Auxiliary>
       );
 
@@ -100,7 +105,8 @@ const mapStateToProps = state => {
   return {
     ings: state.burgerBuilder.ingredients,
     price: state.burgerBuilder.totalPrice,
-    error: state.burgerBuilder.error
+    error: state.burgerBuilder.error,
+    isAuthenticated: state.auth.token != null
   }
 };
 
