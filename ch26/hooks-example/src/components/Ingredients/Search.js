ffbdaa2868c021f4aba75bff0250e31ef9c44d15
Sunny Lin <sunnylin9999@gmail.com>
2021-03-19 12:49:35 +0800

    Add check pause enter with Refs & useRef
    Cleaning up with useEffect.

--- ch26/hooks-example/src/components/Ingredients/Search.js
+++ ch26/hooks-example/src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
@@ -6,27 +6,38 @@ import './Search.css';
 const Search = React.memo(props => {
   const { onLoadIngredients } = props;
   const [enteredFilter, setEnteredFilter] = useState('');
+  const inputRef = useRef(null);
+
+  let timer = null;
 
   useEffect(() => {
-    const query =
-      enteredFilter.length === 0
-        ? ''
-        : `?orderBy="title"&equalTo="${enteredFilter}"`;
+    timer = setTimeout(() => {
+      if(enteredFilter === inputRef.current.value) {
+        const query =
+          enteredFilter.length === 0
+            ? ''
+            : `?orderBy="title"&equalTo="${enteredFilter}"`;
 
-    fetch('https://react17-hooks-default-rtdb.firebaseio.com/ingredients.json' + query)
-      .then(res => res.json())
-      .then(responseData => {
-        const loadedIngredients = [];
-        for(const key in responseData) {
-          loadedIngredients.push({
-            id: key,
-            title: responseData[key].title,
-            amount: responseData[key].amount
+        fetch('https://react17-hooks-default-rtdb.firebaseio.com/ingredients.json' + query)
+          .then(res => res.json())
+          .then(responseData => {
+            const loadedIngredients = [];
+            for(const key in responseData) {
+              loadedIngredients.push({
+                id: key,
+                title: responseData[key].title,
+                amount: responseData[key].amount
+              });
+            }
+            onLoadIngredients(loadedIngredients);
           });
-        }
-        onLoadIngredients(loadedIngredients);
-      });
-  }, [enteredFilter, onLoadIngredients]);
+      }
+    }, 500);
+    return () => {
+      // The cleanup function runs when the component gets unmounted
+      clearTimeout(timer);
+    }
+  }, [enteredFilter, onLoadIngredients, inputRef]);
 
   return (
     <section className="search">
@@ -34,6 +45,7 @@ const Search = React.memo(props => {
         <div className="search-input">
           <label>Filter by Title</label>
           <input
+            ref={inputRef}
             type="text"
             value={enteredFilter}
             onChange={event => setEnteredFilter(event.target.value)}
