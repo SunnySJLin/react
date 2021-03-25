ceaaf8a519d982e2e44e7d9732cf3ba607c0e5fe
Sunny Lin <sunnylin9999@gmail.com>
2021-03-25 16:25:10 +0800

    Converting HOC components using React Hooks

--- ch27/burger-builder/src/hoc/withErrorHandler/withErrorHandler.js
+++ ch27/burger-builder/src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,47 +1,47 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Modal from '../../components/UI/Modal/Modal';
 import Auxiliary from '../Auxiliary/Auxiliary';
 
 const withErrorHandler = (WrappedComponent, axios) => {
-  return class extends Component {
-    state = {
-      error: null
-    }
+  return props => {
+    const [error, setError] = useState(null);
 
-    componentWillMount() {
-      this.reqInterceptor = axios.interceptors.request.use(req => {
-        this.setState({error: null}); // clear any errors
-        return req; // request can continue
-      });
+    const reqInterceptor = axios.interceptors.request.use(req => {
+      setError(null); // clear any errors
+      return req; // request can continue
+    });
 
-      this.resInterceptor = axios.interceptors.response.use(res => res, error => {
-        this.setState({error: error});
-      });
-    }
+    const resInterceptor = axios.interceptors.response.use(
+      res => res,
+      err => {
+        setError(err);
+      }
+    );
 
-    componentWillUnmount() {
-      axios.interceptors.request.eject(this.reqInterceptor);
-      axios.interceptors.response.eject(this.resInterceptor);
-    }
+    useEffect(() => { // React will run main function content when the component mounts.
+      return () => {
+        // Return cleanup function to clean up interceptors, when request or response interceptor changes.
+        axios.interceptors.request.eject(reqInterceptor);
+        axios.interceptors.response.eject(resInterceptor);
+      };
+    }, [reqInterceptor, resInterceptor]);
 
-    errorConfirmedHandler = () => {
-      this.setState({error: null}); //reset
-    }
+    const errorConfirmedHandler = () => {
+      setError(null); //reset
+    };
 
-    render() {
-      return (
-        <Auxiliary>
-          <Modal 
-            show={this.state.error} 
-            modalClosed={this.errorConfirmedHandler}>
-            {this.state.error ? this.state.error.message : null}
-          </Modal>
-          <WrappedComponent {...this.props}/>
-        </Auxiliary>
-      );
-    }
-  }
-}
+    return (
+      <Auxiliary>
+        <Modal 
+          show={error} 
+          modalClosed={errorConfirmedHandler}>
+          {error ? error.message : null}
+        </Modal>
+        <WrappedComponent {...props}/>
+      </Auxiliary>
+    );
+  };
+};
 
 export default withErrorHandler;
