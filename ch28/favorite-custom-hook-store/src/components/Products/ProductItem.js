5d990e4f56b62d81792efe054ececef35e2433f7
Sunny Lin <sunnylin9999@gmail.com>
2021-03-30 15:40:38 +0800

    Using custom Store hook to containers and component
    We built is custom Redux like store implementation.
    
    Create a concrete products store based on store setup.
    Using the custom Store hook.
    Remove Redux and React Redux.

--- ch28/favorite-custom-hook-store/src/components/Products/ProductItem.js
+++ ch28/favorite-custom-hook-store/src/components/Products/ProductItem.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
 
 import Card from '../UI/Card';
+import { useStore } from '../../hooks-store/store';
 import './ProductItem.css';
-import { toggleFav } from '../../store/actions/products';
 
 const ProductItem = props => {
-  const dispatch = useDispatch();
+  // To get dispatch function and store to dispatch constant
+  const dispatch = useStore()[1];
 
   const toggleFavHandler = () => {
-    dispatch(toggleFav(props.id));
+    dispatch('TOGGLE_FAV', props.id);
   };
 
   return (
